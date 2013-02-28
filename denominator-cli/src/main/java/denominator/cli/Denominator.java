d3396f4aadd5be796cd41e798b97779f88b58103
adriancole <adrian.f.cole@gmail.com>
2013-02-28 11:09:42 -0800

    added rrset support in cli

--- denominator-cli/src/main/java/denominator/cli/Denominator.java
+++ denominator-cli/src/main/java/denominator/cli/Denominator.java
@@ -1,10 +1,11 @@
 package denominator.cli;
-
+import static com.google.common.collect.Iterators.transform;
 import static com.google.common.io.Closeables.closeQuietly;
 import static denominator.Credentials.ListCredentials.from;
 import static denominator.CredentialsConfiguration.credentials;
 import static denominator.Denominator.create;
 import static denominator.Denominator.listProviders;
+import static java.lang.String.format;
 import io.airlift.command.Cli;
 import io.airlift.command.Cli.CliBuilder;
 import io.airlift.command.Command;
@@ -12,15 +13,20 @@ import io.airlift.command.Help;
 import io.airlift.command.Option;
 import io.airlift.command.OptionType;
 
+import java.net.InetAddress;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 
+import com.google.common.base.Function;
 import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
 
 import denominator.DNSApiManager;
 import denominator.Provider;
+import denominator.model.ResourceRecordSet;
 
 public class Denominator {
     public static void main(String[] args) {
@@ -35,6 +41,11 @@ public class Denominator {
                .withDefaultCommand(ZoneList.class)
                .withCommand(ZoneList.class);
 
+        builder.withGroup("record")
+               .withDescription("manage resource record sets in a zone")
+               .withDefaultCommand(ResourceRecordSetList.class)
+               .withCommand(ResourceRecordSetList.class);
+
         Cli<Runnable> denominatorParser = builder.build();
 
         denominatorParser.parse(args).run();
@@ -46,16 +57,18 @@ public class Denominator {
             System.out.println(providerAndCredentialsTable());
         }
 
+        final static String table = "%-20s %-16s %s%n";
+
         public static String providerAndCredentialsTable() {
             StringBuilder builder = new StringBuilder();
-            builder.append("provider\tcredential type\tcredential parameters\n");
+            
+            builder.append(format(table, "provider", "credential type", "credential parameters"));
             for (Provider provider : listProviders()) {
                 if (provider.getCredentialTypeToParameterNames().isEmpty())
-                    builder.append(provider.getName()).append('\t').append('\n');
+                    builder.append(format("%-20s%n", provider.getName()));
                 for (Entry<String, Collection<String>> entry : provider.getCredentialTypeToParameterNames().asMap()
                         .entrySet()) {
-                    builder.append(provider.getName()).append('\t').append(entry.getKey()).append('\t')
-                            .append(Joiner.on(' ').join(entry.getValue())).append('\n');
+                    builder.append(format(table, provider.getName(), entry.getKey(), Joiner.on(' ').join(entry.getValue())));
                 }
             }
             return builder.toString();
@@ -94,5 +107,41 @@ public class Denominator {
         public Iterator<String> doRun(DNSApiManager mgr) {
             return mgr.getApi().getZoneApi().list();
         }
+    }
+
+    public static abstract class ResourceRecordSetCommand extends DenominatorCommand {
+        @Option(type = OptionType.GLOBAL, required = true, name = { "-z", "--zone" }, description = "zone name to affect")
+        public String zoneName;
+    }
+
+    @Command(name = "list", description = "Lists the record record sets present in this zone")
+    public static class ResourceRecordSetList extends ResourceRecordSetCommand {
+        public Iterator<String> doRun(DNSApiManager mgr) {
+            Iterator<ResourceRecordSet<?>> list = mgr.getApi().getResourceRecordSetApiForZone(zoneName).list();
+            return transform(list, ResourceRecordSetToString.INSTANCE);
+        }
+    }
+
+    private static enum ResourceRecordSetToString implements Function<ResourceRecordSet<?>, String> {
+        INSTANCE;
+
+        @Override
+        public String apply(ResourceRecordSet<?> input) {
+            StringBuilder lines = new StringBuilder();
+            for (Map<String, Object> rdata : input) {
+                lines.append(format("%-50s %-5s %-6s %s%n", input.getName(), input.getType(), input.getTTL().orNull(),
+                        flatten(rdata)));
+            }
+            return lines.toString();
+        }
+    }
+
+    private static String flatten(Map<String, Object> input) {
+        ImmutableList<Object> orderedRdataValues = ImmutableList.copyOf(input.values());
+        if (orderedRdataValues.size() == 1) {
+            Object rdata = orderedRdataValues.get(0);
+            return rdata instanceof InetAddress ? InetAddress.class.cast(rdata).getHostAddress() :rdata.toString();
+        }
+        return Joiner.on(' ').join(input.values());
     }
 }
