22554a17aac584d0ff65c92d5d67ed277f3e52f1
adriancole <adrian.f.cole@gmail.com>
2013-04-28 14:09:50 -0700

    issue #127: base tests and mock implementation for GeoResourceRecordSetApi.applyTTLToNameTypeAndGroup

---
+++ denominator-core/src/test/java/denominator/mock/MockGeoWriteCommandsLiveTest.java
@@ -0,0 +1,16 @@
+package denominator.mock;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import denominator.profile.BaseGeoWriteCommandsLiveTest;
+
+@Test
+public class MockGeoWriteCommandsLiveTest extends BaseGeoWriteCommandsLiveTest {
+    @BeforeClass
+    private void setUp() {
+        MockConnection connection = new MockConnection();
+        manager = connection.manager;
+        mutableGeoRRSet = connection.mutableGeoRRSet;
+    }
+}
