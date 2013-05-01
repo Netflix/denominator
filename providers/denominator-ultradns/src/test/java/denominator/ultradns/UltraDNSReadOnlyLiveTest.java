21659469deeae14333480a57f3c21a6c4e4513e4
adriancole <adrian.f.cole@gmail.com>
2013-04-30 21:09:00 -0700

    use ultradns list by name and type operations instead of client-side filtering

--- providers/denominator-ultradns/src/test/java/denominator/ultradns/UltraDNSReadOnlyLiveTest.java
+++ providers/denominator-ultradns/src/test/java/denominator/ultradns/UltraDNSReadOnlyLiveTest.java
@@ -1,14 +1,29 @@
 package denominator.ultradns;
 
+import static java.util.logging.Logger.getAnonymousLogger;
+import static org.testng.Assert.assertEquals;
+
+import org.jclouds.ultradns.ws.UltraDNSWSResponseException;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import denominator.BaseReadOnlyLiveTest;
+import denominator.model.ResourceRecordSet;
 
 @Test
 public class UltraDNSReadOnlyLiveTest extends BaseReadOnlyLiveTest {
     @BeforeClass
     private void setUp() {
         manager = new UltraDNSConnection().manager;
+    }
+
+    @Override
+    protected void checkListByNameAndTypeConsistent(String zoneName, ResourceRecordSet<?> rrs) {
+        try {
+            super.checkListByNameAndTypeConsistent(zoneName, rrs);
+        } catch (UltraDNSWSResponseException e) {
+            assertEquals(e.getError().getCode(), 2114);
+            getAnonymousLogger().warning("invalid hostname in record set: " + rrs);
+        }
     }
 }
