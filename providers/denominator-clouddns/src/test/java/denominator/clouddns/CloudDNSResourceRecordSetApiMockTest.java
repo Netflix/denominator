56e312adcb2cc11c86484c36434758fb667d857a
Everett Toews <everett.toews@rackspace.com>
2013-05-25 16:04:10 -0500

    Update to jclouds-1.6.1-SNAPSHOT to test Rackspace Cloud DNS pagination fix. Added tests.

--- providers/denominator-clouddns/src/test/java/denominator/clouddns/CloudDNSResourceRecordSetApiMockTest.java
+++ providers/denominator-clouddns/src/test/java/denominator/clouddns/CloudDNSResourceRecordSetApiMockTest.java
@@ -31,6 +31,104 @@ public class CloudDNSResourceRecordSetApiMockTest {
 
     String session = "{\"access\":{\"token\":{\"id\":\"b84f4a37-5126-4603-9521-ccd0665fbde1\",\"expires\":\"2013-04-13T16:49:57.000-05:00\",\"tenant\":{\"id\":\"123123\",\"name\":\"123123\"}},\"serviceCatalog\":[{\"endpoints\":[{\"tenantId\":\"123123\",\"publicURL\":\"URL/v1.0/123123\"}],\"name\":\"cloudMonitoring\",\"type\":\"rax:monitor\"},{\"endpoints\":[{\"region\":\"DFW\",\"tenantId\":\"MossoCloudFS_5bcf396e-39dd-45ff-93a1-712b9aba90a9\",\"publicURL\":\"URL/v1/MossoCloudFS_5bcf396e-39dd-45ff-93a1-712b9aba90a9\"},{\"region\":\"ORD\",\"tenantId\":\"MossoCloudFS_5bcf396e-39dd-45ff-93a1-712b9aba90a9\",\"publicURL\":\"URL/v1/MossoCloudFS_5bcf396e-39dd-45ff-93a1-712b9aba90a9\"}],\"name\":\"cloudFilesCDN\",\"type\":\"rax:object-cdn\"},{\"endpoints\":[{\"region\":\"ORD\",\"tenantId\":\"123123\",\"publicURL\":\"URL/v1.0/123123\"},{\"region\":\"DFW\",\"tenantId\":\"123123\",\"publicURL\":\"URL/v1.0/123123\"}],\"name\":\"cloudLoadBalancers\",\"type\":\"rax:load-balancer\"},{\"endpoints\":[{\"region\":\"DFW\",\"tenantId\":\"123123\",\"publicURL\":\"URL/v1.0/123123\"},{\"region\":\"ORD\",\"tenantId\":\"123123\",\"publicURL\":\"URL/v1.0/123123\"}],\"name\":\"cloudDatabases\",\"type\":\"rax:database\"},{\"endpoints\":[{\"region\":\"DFW\",\"tenantId\":\"MossoCloudFS_5bcf396e-39dd-45ff-93a1-712b9aba90a9\",\"publicURL\":\"URL/v1/MossoCloudFS_5bcf396e-39dd-45ff-93a1-712b9aba90a9\",\"internalURL\":\"URL/v1/MossoCloudFS_5bcf396e-39dd-45ff-93a1-712b9aba90a9\"},{\"region\":\"ORD\",\"tenantId\":\"MossoCloudFS_5bcf396e-39dd-45ff-93a1-712b9aba90a9\",\"publicURL\":\"URL/v1/MossoCloudFS_5bcf396e-39dd-45ff-93a1-712b9aba90a9\",\"internalURL\":\"URL/v1/MossoCloudFS_5bcf396e-39dd-45ff-93a1-712b9aba90a9\"}],\"name\":\"cloudFiles\",\"type\":\"object-store\"},{\"endpoints\":[{\"tenantId\":\"123123\",\"publicURL\":\"URL/v1.0/123123\",\"versionInfo\":\"URL/v1.0\",\"versionList\":\"URL/\",\"versionId\":\"1.0\"}],\"name\":\"cloudServers\",\"type\":\"compute\"},{\"endpoints\":[{\"region\":\"DFW\",\"tenantId\":\"123123\",\"publicURL\":\"URL/v2/123123\",\"versionInfo\":\"URL/v2\",\"versionList\":\"URL/\",\"versionId\":\"2\"},{\"region\":\"ORD\",\"tenantId\":\"123123\",\"publicURL\":\"URL/v2/123123\",\"versionInfo\":\"URL/v2\",\"versionList\":\"URL/\",\"versionId\":\"2\"}],\"name\":\"cloudServersOpenStack\",\"type\":\"compute\"},{\"endpoints\":[{\"tenantId\":\"123123\",\"publicURL\":\"URL/v1.0/123123\"}],\"name\":\"cloudDNS\",\"type\":\"rax:dns\"},{\"endpoints\":[{\"tenantId\":\"123123\",\"publicURL\":\"URL/v1.0/123123\"}],\"name\":\"cloudBackup\",\"type\":\"rax:backup\"},{\"endpoints\":[{\"region\":\"DFW\",\"tenantId\":\"123123\",\"publicURL\":\"URL/v1/123123\"},{\"region\":\"ORD\",\"tenantId\":\"123123\",\"publicURL\":\"URL/v1/123123\"}],\"name\":\"cloudBlockStorage\",\"type\":\"volume\"}],\"user\":{\"id\":\"1234\",\"roles\":[{\"id\":\"3\",\"description\":\"User Admin Role.\",\"name\":\"identity:user-admin\"}],\"name\":\"jclouds-joe\",\"RAX-AUTH:defaultRegion\":\"DFW\"}}}";
     String domains = "{\"domains\": [{\"name\": \"denominator.io\",\"id\": 1234,\"comment\": \"Hello dev subdomain\",\"accountId\": 123123,\"emailAddress\": \"contact@denominator.io\",\"updated\": \"2013-03-22T03:04:15.000+0000\",\"created\": \"2013-03-22T03:04:15.000+0000\"}]}";
+    String records = "{\"records\":[{\"name\":\"www.denominator.io\",\"id\":\"A-9872761\",\"type\":\"A\",\"data\":\"1.2.3.4\",\"ttl\":600000,\"updated\":\"2013-04-13T14:42:00.000+0000\",\"created\":\"2013-04-13T14:42:00.000+0000\"},{\"name\":\"www.denominator.io\",\"id\":\"NS-8703385\",\"type\":\"NS\",\"data\":\"dns1.stabletransit.com\",\"ttl\":600000,\"updated\":\"2013-04-13T14:42:00.000+0000\",\"created\":\"2013-04-13T14:42:00.000+0000\"},{\"name\":\"www.denominator.io\",\"id\":\"NS-8703386\",\"type\":\"NS\",\"data\":\"dns2.stabletransit.com\",\"ttl\":600000,\"updated\":\"2013-04-13T14:42:00.000+0000\",\"created\":\"2013-04-13T14:42:00.000+0000\"}],\"totalEntries\":3}";
+
+    @Test
+    public void listWhenPresent() throws IOException, InterruptedException {
+        MockWebServer server = new MockWebServer();
+        server.play();
+
+        URL url = server.getUrl("/");
+        server.setDispatcher(getURLReplacingQueueDispatcher(url));
+
+        server.enqueue(new MockResponse().setResponseCode(OK.getStatusCode()).setBody(session));
+        server.enqueue(new MockResponse().setResponseCode(OK.getStatusCode()).setBody(domains));
+        server.enqueue(new MockResponse().setResponseCode(OK.getStatusCode()).setBody(records));
+
+        try {
+            ResourceRecordSetApi api = mockApi(url);
+            Iterator<ResourceRecordSet<?>> records = api.list();
+            
+            while (records.hasNext()) {
+                ResourceRecordSet<?> record = records.next();
+                
+                assertEquals(record.getName(), "www.denominator.io");
+                assertEquals(record.getTTL().get().intValue(), 600000);
+            }
+
+            assertEquals(server.getRequestCount(), 3);
+            assertEquals(server.takeRequest().getRequestLine(), "POST /tokens HTTP/1.1");
+            assertEquals(server.takeRequest().getRequestLine(), "GET /domains HTTP/1.1");
+            assertEquals(server.takeRequest().getRequestLine(), "GET /domains/1234/records HTTP/1.1");
+        } finally {
+            server.shutdown();
+        }
+    }
+
+    @Test
+    public void listWhenAbsent() throws IOException, InterruptedException {
+        MockWebServer server = new MockWebServer();
+        server.play();
+
+        URL url = server.getUrl("/");
+        server.setDispatcher(getURLReplacingQueueDispatcher(url));
+
+        server.enqueue(new MockResponse().setResponseCode(OK.getStatusCode()).setBody(session));
+        server.enqueue(new MockResponse().setResponseCode(OK.getStatusCode()).setBody(domains));
+        server.enqueue(new MockResponse().setResponseCode(NOT_FOUND.getStatusCode())); // no existing records
+
+        try {
+            ResourceRecordSetApi api = mockApi(url);
+            
+            assertFalse(api.list().hasNext());
+            assertEquals(server.getRequestCount(), 3);
+            assertEquals(server.takeRequest().getRequestLine(), "POST /tokens HTTP/1.1");
+            assertEquals(server.takeRequest().getRequestLine(), "GET /domains HTTP/1.1");
+            assertEquals(server.takeRequest().getRequestLine(), "GET /domains/1234/records HTTP/1.1");
+        } finally {
+            server.shutdown();
+        }
+    }
+    
+    String recordsPage1 = "{\"records\":[{\"name\":\"www.denominator.io\",\"id\":\"A-9872761\",\"type\":\"A\",\"data\":\"1.2.3.4\",\"ttl\":600000,\"updated\":\"2013-04-13T14:42:00.000+0000\",\"created\":\"2013-04-13T14:42:00.000+0000\"},{\"name\":\"www.denominator.io\",\"id\":\"NS-8703385\",\"type\":\"NS\",\"data\":\"dns1.stabletransit.com\",\"ttl\":600000,\"updated\":\"2013-04-13T14:42:00.000+0000\",\"created\":\"2013-04-13T14:42:00.000+0000\"},{\"name\":\"www.denominator.io\",\"id\":\"NS-8703386\",\"type\":\"NS\",\"data\":\"dns2.stabletransit.com\",\"ttl\":600000,\"updated\":\"2013-04-13T14:42:00.000+0000\",\"created\":\"2013-04-13T14:42:00.000+0000\"}],\"totalEntries\":3,\"links\":[{\"href\":\"https://dns.api.rackspacecloud.com/v1.0/123123/domains/1234/records?limit=3&offset=3\",\"rel\":\"next\"}]}";
+    String recordsPage2 = "{\"records\":[{\"name\":\"www.denominator.io\",\"id\":\"A-9872761\",\"type\":\"A\",\"data\":\"1.2.3.4\",\"ttl\":600000,\"updated\":\"2013-04-13T14:42:00.000+0000\",\"created\":\"2013-04-13T14:42:00.000+0000\"},{\"name\":\"www.denominator.io\",\"id\":\"NS-8703385\",\"type\":\"NS\",\"data\":\"dns1.stabletransit.com\",\"ttl\":600000,\"updated\":\"2013-04-13T14:42:00.000+0000\",\"created\":\"2013-04-13T14:42:00.000+0000\"},{\"name\":\"www.denominator.io\",\"id\":\"NS-8703386\",\"type\":\"NS\",\"data\":\"dns2.stabletransit.com\",\"ttl\":600000,\"updated\":\"2013-04-13T14:42:00.000+0000\",\"created\":\"2013-04-13T14:42:00.000+0000\"}],\"totalEntries\":3,\"links\":[{\"href\":\"https://dns.api.rackspacecloud.com/v1.0/123123/domains/1234/records?limit=3&offset=0\",\"rel\":\"previous\"}]}";
+
+    @Test
+    public void listPagesWhenPresent() throws IOException, InterruptedException {
+        MockWebServer server = new MockWebServer();
+        server.play();
+
+        URL url = server.getUrl("/");
+        server.setDispatcher(getURLReplacingQueueDispatcher(url));
+
+        server.enqueue(new MockResponse().setResponseCode(OK.getStatusCode()).setBody(session));
+        server.enqueue(new MockResponse().setResponseCode(OK.getStatusCode()).setBody(domains));
+        server.enqueue(new MockResponse().setResponseCode(OK.getStatusCode()).setBody(recordsPage1));
+        server.enqueue(new MockResponse().setResponseCode(OK.getStatusCode()).setBody(recordsPage2));
+
+        try {
+            ResourceRecordSetApi api = mockApi(url);
+            Iterator<ResourceRecordSet<?>> records = api.list();
+            
+            while (records.hasNext()) {
+                ResourceRecordSet<?> record = records.next();
+                
+                assertEquals(record.getName(), "www.denominator.io");
+                assertEquals(record.getTTL().get().intValue(), 600000);
+            }
+
+            assertEquals(server.getRequestCount(), 4);
+            assertEquals(server.takeRequest().getRequestLine(), "POST /tokens HTTP/1.1");
+            assertEquals(server.takeRequest().getRequestLine(), "GET /domains HTTP/1.1");
+            assertEquals(server.takeRequest().getRequestLine(), "GET /domains/1234/records HTTP/1.1");
+            assertEquals(server.takeRequest().getRequestLine(), "GET /domains/1234/records?limit=3&offset=3 HTTP/1.1");
+        } finally {
+            server.shutdown();
+        }
+    }
+
+    
+    
     String recordsByName = "{\"records\":[{\"name\":\"www.denominator.io\",\"id\":\"A-9872761\",\"type\":\"A\",\"data\":\"1.2.3.4\",\"ttl\":600000,\"updated\":\"2013-04-13T14:42:00.000+0000\",\"created\":\"2013-04-13T14:42:00.000+0000\"},{\"name\":\"www.denominator.io\",\"id\":\"NS-8703385\",\"type\":\"NS\",\"data\":\"dns1.stabletransit.com\",\"ttl\":600000,\"updated\":\"2013-04-13T14:42:00.000+0000\",\"created\":\"2013-04-13T14:42:00.000+0000\"},{\"name\":\"www.denominator.io\",\"id\":\"NS-8703386\",\"type\":\"NS\",\"data\":\"dns2.stabletransit.com\",\"ttl\":600000,\"updated\":\"2013-04-13T14:42:00.000+0000\",\"created\":\"2013-04-13T14:42:00.000+0000\"}],\"totalEntries\":3}";
 
     @Test
