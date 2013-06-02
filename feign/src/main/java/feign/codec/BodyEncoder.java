488e4ccbca2160f513a9810510c072b384d7672f
adriancole <adrian.f.cole@gmail.com>
2013-06-02 12:19:56 -0700

    Feign makes developing against apis that feign restfulness a little bit easier.

---
+++ feign/src/main/java/feign/codec/BodyEncoder.java
@@ -0,0 +1,43 @@
+package feign.codec;
+
+import feign.RequestTemplate;
+
+public interface BodyEncoder {
+    /**
+     * Converts objects to an appropriate representation. Can affect any part of
+     * {@link RequestTemplate}.
+     * <p/>
+     * Ex.
+     * 
+     * <pre>
+     * public class GsonEncoder implements BodyEncoder {
+     *     private final Gson gson;
+     * 
+     *     public GsonEncoder(Gson gson) {
+     *         this.gson = gson;
+     *     }
+     * 
+     *     &#064;Override
+     *     public void encodeBody(Object bodyParam, RequestTemplate base) {
+     *         base.body(gson.toJson(bodyParam));
+     *     }
+     * 
+     * }
+     * </pre>
+     * 
+     * If a parameter has no {@code *Param} annotation, it is passed to this
+     * method.
+     * 
+     * <pre>
+     * &#064;POST
+     * &#064;Path(&quot;/&quot;)
+     * void create(User user);
+     * </pre>
+     * 
+     * @param bodyParam
+     *            a body parameter
+     * @param base
+     *            template to encode the {@code object} into.
+     */
+    void encodeBody(Object bodyParam, RequestTemplate base);
+}
