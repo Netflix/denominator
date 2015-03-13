ec04668ba6954ce03d55c6b35e2f6986eaae2543
Adrian Cole <adrian.f.cole@gmail.com>
2015-03-13 11:43:03 -0700

    Updates to Feign 8.1.0

--- ultradns/src/main/java/denominator/ultradns/UltraDNSTarget.java
+++ ultradns/src/main/java/denominator/ultradns/UltraDNSTarget.java
@@ -18,21 +18,21 @@ import static java.lang.String.format;
 
 class UltraDNSTarget implements Target<UltraDNS> {
 
-  static final String SOAP_TEMPLATE = ""
-                                      + "<?xml version=\"1.0\"?>\n"
-                                      + "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:v01=\"http://webservice.api.ultra.neustar.com/v01/\">\n"
-                                      + "  <soapenv:Header>\n"
-                                      + "    <wsse:Security xmlns:wsse=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\" soapenv:mustUnderstand=\"1\">\n"
-                                      + "      <wsse:UsernameToken>\n"
-                                      + "        <wsse:Username>%s</wsse:Username>\n"
-                                      + "        <wsse:Password Type=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText\">%s</wsse:Password>\n"
-                                      + "      </wsse:UsernameToken>\n"
-                                      + "    </wsse:Security>\n"
-                                      + "  </soapenv:Header>\n"
-                                      + "  <soapenv:Body>\n"
-                                      + "    %s\n"
-                                      + "  </soapenv:Body>\n"
-                                      + "</soapenv:Envelope>";
+    static final String SOAP_TEMPLATE = "<?xml version=\"1.0\"?>\n"
+            + "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:v01=\"http://webservice.api.ultra.neustar.com/v01/\">\n"
+            + "  <soapenv:Header>\n"
+            + "    <wsse:Security xmlns:wsse=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\" soapenv:mustUnderstand=\"1\">\n"
+            + "      <wsse:UsernameToken>\n"
+            + "        <wsse:Username>%s</wsse:Username>\n"
+            + "        <wsse:Password Type=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText\">%s</wsse:Password>\n"
+            + "      </wsse:UsernameToken>\n"
+            + "    </wsse:Security>\n"
+            + "  </soapenv:Header>\n"
+            + "  <soapenv:Body>\n"
+            + "    %s\n"
+            + "  </soapenv:Body>\n"
+            + "</soapenv:Envelope>";
+
   private final Provider provider;
   private final javax.inject.Provider<Credentials> credentials;
 
@@ -79,7 +79,6 @@ class UltraDNSTarget implements Target<UltraDNS> {
     in.insert(0, url());
     in.body(format(SOAP_TEMPLATE, username, password, new String(in.body(), UTF_8)));
     in.header("Host", URI.create(in.url()).getHost());
-    in.header("Content-Type", "application/xml");
     return in.request();
   }
 }
