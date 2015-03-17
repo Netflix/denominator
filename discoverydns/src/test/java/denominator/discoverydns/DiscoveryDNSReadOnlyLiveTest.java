80a3067353af7bbd3f798d475fe3ffa815545270
Arnaud Dumont <arnaud.dumont@ausregistry.com.au>
2015-03-17 14:30:41 +1100

    Remove support for DiscoveryDNS

--- discoverydns/src/test/java/denominator/discoverydns/DiscoveryDNSReadOnlyLiveTest.java
+++
@@ -1,9 +0,0 @@
-package denominator.discoverydns;
-
-import denominator.Live.UseTestGraph;
-import denominator.ReadOnlyLiveTest;
-
-@UseTestGraph(DiscoveryDNSTestGraph.class)
-public class DiscoveryDNSReadOnlyLiveTest extends ReadOnlyLiveTest {
-
-}
--- discoverydns/src/test/java/denominator/discoverydns/DiscoveryDNSWriteCommandsLiveTest.java
+++
@@ -1,9 +0,0 @@
-package denominator.discoverydns;
-
-import denominator.Live.UseTestGraph;
-import denominator.WriteCommandsLiveTest;
-
-@UseTestGraph(DiscoveryDNSTestGraph.class)
-public class DiscoveryDNSWriteCommandsLiveTest extends WriteCommandsLiveTest {
-
-}
